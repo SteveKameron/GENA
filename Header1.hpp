77b83aed1ec3eaad980a9660b3310f0c53b8ece5
Steve Kameron <scorching.k173@gmail.com>
2023-12-05 19:23:13 +0300

    Comments 2

--- Header1.hpp
+++ Header1.hpp
@@ -6,13 +6,13 @@ extern int Coef[11];
 // For what function we search root
 double targetFunction(double x) {
     //return 2 * pow(x, 7) - 163 * pow(x, 6) + 3702 * pow(x, 5) - 36056 * pow(x, 4) + 167128 * pow(x, 3) - 344133 * x * x + 174528 * x + 196560;
-    return x - 2000;
+    //return x - 2000;
     //return Coef[0] * pow(x, 10) + Coef[1] * pow(x, 9) + Coef[2] * pow(x, 8) + Coef[3] * pow(x, 7) + Coef[4] * pow(x, 6) + Coef[5] * pow(x, 5) + Coef[6] * pow(x, 4) + Coef[7] * pow(x, 3) + Coef[8] * pow(x, 2) + Coef[9] * pow(x, 1) + Coef[10];
     //return (x - 52) * (x - 12) * (x - 7) * (x - 5) * (x - 3) * (x - 3) * (2 * x + 1);
 }
 struct Genome {
-    double value; // √еном (представл€ет собой значение x)
-    double fitness; // ѕриспособленность (близость к решению)
+    double value; // Genom
+    double fitness; // Fitness
 
     // Constructor to create new Genom
     Genome() : value((rand() % 20000 - 10000) / 100.0), fitness(0.0) {}
